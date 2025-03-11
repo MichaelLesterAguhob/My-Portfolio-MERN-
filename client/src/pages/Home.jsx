2ce103fd282a28f19dc175f446b78a47754dc1a3
michaellesteraguhob023 <michaellesteraguhob023@gmail.com>
2025-03-11 16:03:01 +0800

    Changes on home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,17 +22,17 @@ const Home = () => {
       id="home"
       className="min-h-[95vh] mt-[65px] p-2 flex flex-col justify-center items-center md:grid grid-flow-col grid-cols-5"
     >
-      <div className="w-full md:h-2/4 md:col-span-2 flex justify-center md:justify-end items-end md:items-center p-4 overflow-hidden">
+      <div className="w-full md:col-span-2 flex justify-center md:justify-end items-end md:items-center p-4 overflow-hidden">
         <img src={MyPhoto} alt='Michael Photo' className="w-full h-auto max-w-[340px] max-h-[340px]"/>
       </div>  
 
-      <div className="w-full md:h-2/4 md:col-span-3 flex flex-col justify-center  md:items-center overflow-hidden">
+      <div className="w-full flex flex-col md:col-span-3  md:justify-center md:items-start overflow-hidden">
         <div className="w-full h-fit flex justify-start">
           <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white text-[min(6vw,50px)] md:text-[min(4vw,40px)] lg:text-[min(4vw,50px)] text-white font-bold text-start select-none p-2 ">
             {text} &nbsp;
           </h1>
         </div>
-        <p className="w-full p-2 text-white text-2xl text-justify">Hi👋 I'm Michael and I am desktop software and web developer and <strong>I'm open for an entry level job.</strong> I love coding, building a software or website that let me turned ideas into reality.</p>
+        <p className="w-full md:w-3/4 p-2 text-white text-2xl text-justify">Hi👋 I'm Michael and I am desktop software and web developer and <strong>I'm open for an entry level job.</strong> I love coding, building a software or website that let me turned ideas into reality.</p>
       </div>
     </div>
   );
