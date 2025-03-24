3003975424f6c329201390730e8378b6517bb10c
michaellesteraguhob023 <michaellesteraguhob023@gmail.com>
2025-03-24 16:07:40 +0800

    Updated env for gdrive integration

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -14,16 +14,31 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ 
+const upload = multer({
   storage: storage,
- }).array("photos", 10);
-
+}).array("photos", 10);
 
 module.exports.upload = upload;
 
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Convert `\n` to actual newlines
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
+
+// console.log(credentials);
+ 
 // Gdrive Authentication
 const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
+  credentials,
   scopes: ["https://www.googleapis.com/auth/drive.file"],
 });
 
